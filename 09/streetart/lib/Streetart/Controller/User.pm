454f2bfbe98505aa3a984f541b084c588c2f1c5b
reneeb <github@renee-baecker.de>
2015-07-13 23:13:22 +0200

    uebung 9

---
+++ 09/streetart/lib/Streetart/Controller/User.pm
@@ -0,0 +1,29 @@
+package Streetart::Controller::User;
+use Mojo::Base 'Mojolicious::Controller';
+
+# This action will render a template
+sub upload {
+  my $self = shift;
+
+  $self->render();
+}
+
+sub upload_do {
+  my $self = shift;
+
+  $self->render();
+}
+
+sub comment {
+  my $self = shift;
+
+  $self->render();
+}
+
+sub comment {
+  my $self = shift;
+
+  $self->render();
+}
+
+1;
