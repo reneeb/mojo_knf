a52cd041a1b9b8d96538e9f28bc1b840ca072441
reneeb <github@renee-baecker.de>
2015-07-13 17:37:29 +0200

    more updates

--- 04/streetart/lib/Streetart/Controller/Guest.pm
+++ 04/streetart/lib/Streetart/Controller/Guest.pm
@@ -1,17 +1,23 @@
 package Streetart::Controller::Guest;
 use Mojo::Base 'Mojolicious::Controller';
 
+use Time::Piece;
+
 # This action will render a template
 sub welcome {
   my $self = shift;
 
-  $self->render();
+  my $time = localtime;
+
+  $self->render( year => $time->year );
 }
 
 sub contact {
   my $self = shift;
 
-  $self->render();
+  my $time = localtime;
+
+  $self->render( year => $time->year );
 }
 
 sub contact_send {
